9c3637d31d44b0d5f46acd0519a7f681b5dfd495
EmmaDann <e.dann@students.uu.nl>
2017-11-23 13:28:08 +0100

    commit message

--- mergeCov2c.py
+++ mergeCov2c.py
@@ -14,25 +14,6 @@ for file in os.listdir(dir):
     if fnmatch.fnmatch(file, '*.CpG_report.txt.gz'):
         files.append(dir+"/"+file)
 
-
-# cov2c = pd.read_csv(dir+"/"+files[0], sep="\t", header=None) 
-# cov2c.columns = ["chr", "pos", "strand", "C", "T", "context", "flank"]
-
-# for file in files[1:]:
-# 	cov2c_2 = pd.read_csv(dir+"/"+file, sep="\t", header=None) 
-# 	cov2c_2.columns = ["chr", "pos", "strand", "C", "T", "context", "flank"]
-# 	## gives MemoryError --> ask Buys
-# 	df=pd.merge(cov2c,cov2c_2,how="outer",on=["chr","pos","strand","context", "flank"] ).fillna(0)
-# 	df=df.assign(C=df.C_x+df.C_y, T=df.T_x+df.T_y)
-# 	cov2c=df[["chr","pos","strand","C","T","context","flank"]]
-
-# # average values
-# cov2c_avg=cov2c.assign(T=df["T"]/len(files), C=df.C/len(files))
-# cov2c_avg=cov2c_avg.assign(frac=cov2c_avg.C/(cov2c_avg.C+cov2c_avg['T']))
-
-# cov2c_avg.to_csv(dir+"/cov2c_merged_refGen.csv", sep='\t')
-
-
 cov_dict=collections.OrderedDict()
 with gzip.open(files[0], "rb") as f:
 	for line in f:
@@ -50,3 +31,22 @@ for key,val in cov_dict.items():
 	list_key=key.split("\t")
 	newLine =  list_key[:3]+[str(i/2) for i in val]+list_key[3:]
 	print('\t'.join(newLine))
+
+
+## Pandas version (MemoryError)
+# cov2c = pd.read_csv(dir+"/"+files[0], sep="\t", header=None) 
+# cov2c.columns = ["chr", "pos", "strand", "C", "T", "context", "flank"]
+
+# for file in files[1:]:
+# 	cov2c_2 = pd.read_csv(dir+"/"+file, sep="\t", header=None) 
+# 	cov2c_2.columns = ["chr", "pos", "strand", "C", "T", "context", "flank"]
+# 	## gives MemoryError --> ask Buys
+# 	df=pd.merge(cov2c,cov2c_2,how="outer",on=["chr","pos","strand","context", "flank"] ).fillna(0)
+# 	df=df.assign(C=df.C_x+df.C_y, T=df.T_x+df.T_y)
+# 	cov2c=df[["chr","pos","strand","C","T","context","flank"]]
+
+# # average values
+# cov2c_avg=cov2c.assign(T=df["T"]/len(files), C=df.C/len(files))
+# cov2c_avg=cov2c_avg.assign(frac=cov2c_avg.C/(cov2c_avg.C+cov2c_avg['T']))
+
+# cov2c_avg.to_csv(dir+"/cov2c_merged_refGen.csv", sep='\t')
