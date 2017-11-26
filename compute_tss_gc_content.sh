3c97821a84d4daf494d6cfeb1334875a3013a780
EmmaDann <e.dann@students.uu.nl>
2017-11-26 15:51:54 +0100

    commit message

--- compute_tss_gc_content.sh
+++ compute_tss_gc_content.sh
@@ -9,4 +9,4 @@ cat hgTables.refgen.${org}.txt |
 	tr ' ' '\t' | 
 	tail -n+2 | 
 	grep -v '_'| 
-	/hpc/hub_oudenaarden/edann/bin/bedtools2/bin/bedtools slop -b $slop -i stdin -g ../genomes/${org}/${org}.genome | /hpc/hub_oudenaarden/edann/bin/bedtools2/bin/bedtools nuc -fi ../genomes/${org}/${org}.fa -bed stdin | cut -f 1,2,3,5 > GCcont_txEnd_${flank}slop_hgTables.refgen.${org}.txt
+	/hpc/hub_oudenaarden/edann/bin/bedtools2/bin/bedtools slop -b $slop -i stdin -g ../genomes/${org}/${org}.genome | /hpc/hub_oudenaarden/edann/bin/bedtools2/bin/bedtools nuc -fi ../genomes/${org}/${org}.fa -bed stdin | cut -f 1,2,3,5 
