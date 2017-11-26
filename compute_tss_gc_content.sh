aad8e8dc49ff6afcc2c748c6d98dc9eaa0c7052d
EmmaDann <e.dann@students.uu.nl>
2017-11-26 09:14:45 +0100

    commit message

--- compute_tss_gc_content.sh
+++ compute_tss_gc_content.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat hgTables.refgen.mm10.txt | awk '{print $3,$5,$5}' | tr ' ' '\t' | tail -n+2 | grep -v '_'| /hpc/hub_oudenaarden/edann/bin/bedtools2/bin/bedtools slop -b 500 -i stdin -g ../genomes/mm10/mm10.genome | /hpc/hub_oudenaarden/edann/bin/bedtools2/bin/bedtools nuc -fi ../genomes/mm10/mm10.fa -bed stdin | cut -f 1,2,3,5 > GCcont_1000slop_hgTables.refgen.dr10.txt
+zcat refGen.hg38.txt.gz | awk '{print $3,$5,$5}' | tr ' ' '\t' | tail -n+2 | grep -v '_'| /hpc/hub_oudenaarden/edann/bin/bedtools2/bin/bedtools slop -b 500 -i stdin -g ../genomes/hg38.genome | /hpc/hub_oudenaarden/edann/bin/bedtools2/bin/bedtools nuc -fi ../genomes/hg38.fa -bed stdin | cut -f 1,2,3,5 > GCcont_1000slop_hgTables.refgen.hg38.txt
--- sumChrCounts.py
+++ sumChrCounts.py
@@ -12,12 +12,12 @@ import fnmatch
 dir='/home/emma/mnt/edann/hexamers'
 # dir="/hpc/hub_oudenaarden/edann/hexamers"
 files=[]
-# for file in os.listdir(dir):
-#     if fnmatch.fnmatch(file, '*convRef.chr*'):
-#         files.append(dir+"/"+file)
-with open(dir+"/"+"files.txt",'rb') as f:
-	for file in f.readlines():
-		files.append(dir+"/"+file.strip("\n"))
+for file in os.listdir(dir):
+    if fnmatch.fnmatch(file, 'count_noNan_chr*'):
+        files.append(dir+"/"+file)
+# with open(dir+"/"+"files.txt",'rb') as f:
+# 	for file in f.readlines():
+# 		files.append(dir+"/"+file.strip("\n"))
 
 hexAbundance=collections.Counter()
 for file in files:
@@ -27,4 +27,5 @@ for file in files:
 			hexAbundance[aline[0]]+=float(aline[1])
 
 for kmer, abundance in hexAbundance.most_common(): # sorts by abundance
-	print(f"{kmer}\t{abundance}")
+	# print(f"{kmer}\t{abundance}")
+	print kmer, abundance
