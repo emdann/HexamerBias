49c4b90b3cdedf1f142553f956d537c68ea627c2
EmmaDann <e.dann@students.uu.nl>
2017-11-27 18:05:05 +0100

    commit message

--- subsampling.sh
+++ subsampling.sh
@@ -6,3 +6,4 @@ p=$2
 samtools view -s $p $smp > sub_${p}_${smp}
 /hpc/hub_oudenaarden/bin/software/bismark_v0.16.3/bismark_methylation_extractor --samtools_path /hpc/hub_oudenaarden/bdebarbanson/bin/samtools-1.3.1 --comprehensive -s --genome_folder /hpc/hub_oudenaarden/avo/BS/mm10 sub_${p}_${smp}
 rm sub_${p}_${smp}
+
