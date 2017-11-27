3f836ac4aa8603dac6af78bccde7d30ba6e24324
EmmaDann <e.dann@students.uu.nl>
2017-11-27 16:17:22 +0100

    commit message

--- mergeCov2c.py
+++ mergeCov2c.py
@@ -20,14 +20,14 @@ cov_dict=collections.OrderedDict()
 with gzip.open(files[0], "rb") as f:
 	for line in f:
 		aline=line.decode().strip('\n').split('\t')
-		cov_dict['\t'.join([aline[i] for i in [0,1,2,5,6]])]=[int(i) for i in aline[3:5]]
+		cov_dict['\t'.join([aline[i] for i in [0,1,2]])]=[int(i) for i in aline[4:5]]
 
 for file in files[1:]:
 	with gzip.open(file,'rb') as f:
 		for line in f:
 			aline=line.decode().strip('\n').split('\t')
-			cov_dict['\t'.join([aline[i] for i in [0,1,2,5,6]])][0]+= int(aline[3])
-			cov_dict['\t'.join([aline[i] for i in [0,1,2,5,6]])][1]+= int(aline[4])
+			cov_dict['\t'.join([aline[i] for i in [0,1,2]])][0]+= int(aline[4])
+			cov_dict['\t'.join([aline[i] for i in [0,1,2]])][1]+= int(aline[5])
 
 for key,val in cov_dict.items():
 	list_key=key.split("\t")
