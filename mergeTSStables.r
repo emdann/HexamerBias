fea73242a4a4cd7270c5dc0c15f28247f87a6b57
EmmaDann <e.dann@students.uu.nl>
2017-11-28 23:19:31 +0100

    commit message

---
+++ mergeTSStables.r
@@ -0,0 +1,42 @@
+library(dplyr)
+library(data.table)
+library(caTools)
+library(RColorBrewer)
+
+colpal
+
+loadHeaderTSStab<-function(file_path){
+  header<-scan(file_path,what = "", nlines = 1)
+  hex<-substr(header[1],1,4)
+  header[1]<-substr(header[1],5,11)
+  return(header)
+}
+
+TSSdistances<-fread("~/mnt/edann/hexamers/sumTSS_distances.txt", col.names = c("hex", loadHeaderTSStab("~/mnt/edann/hexamers/TSS_tab.chr10.txt")))
+
+# Make prob function
+p_mat <- apply(t(TSSdistances[,-1]),1,function(i) as.numeric(i/sum(i)))
+p_df<-cbind(TSSdistances[,1],p_mat)
+
+sapply(sample(seq_along(p_df$hex),10), function(hex) lines(as.numeric(colnames(p_df)[-1]),runmean(as.numeric(p_df[hex,-1]), k = 100), type='l', main=paste(p_df$hex[hex])))
+
+
+hex.df<-read.csv("~/hex_analysis.csv")
+top_hex<-hex.df[order(hex.df$usage, decreasing = T),]$hex[1:10]
+bot_hex<-hex.df[order(hex.df$usage, decreasing = F),]$hex[1:10]
+
+plotSetProfile<- function(hexs,k=100,title=NULL){
+  cols=rainbow(length(hexs))
+  idx=which(p_df$hex %in% hexs)
+  rm<-apply(p_df[idx,-1],1,runmean,k=k)
+  ylim=c(min(rm),max(rm))
+  plot(as.numeric(colnames(p_df)[-1]),runmean(as.numeric(p_df[idx[1],-1]), k = k), type='l', col=cols[1], ylim=ylim, xlab = "Distance from TSS (bps)", ylab = "Prob")
+  sapply(idx[-1], function(hex) lines(as.numeric(colnames(p_df)[-1]),runmean(as.numeric(p_df[hex,-1]), k = k), type='l', col=cols[which(idx==hex)]))
+  if(!is.null(title)){title(title)}
+  }
+
+pdf("~/HexamerBias/output/TSS_profiles_mostVSleastused.pdf", width = 12)
+par(mfrow=c(1,2))
+plotSetProfile(top_hex, title = "Most used hexamers")
+plotSetProfile(bot_hex, title = "Least used hexamers")
+dev.off()
--- subsampling.sh
+++ subsampling.sh
@@ -1,9 +1,37 @@
-#!/bin/bash 
+import pandas as pd
+import random
+import gzip
+import collections
+import numpy as np
 
-smp=$1
-p=$2
 
-samtools view -b -s $p $smp > sub_${p}_${smp}
-/hpc/hub_oudenaarden/bin/software/bismark_v0.16.3/bismark_methylation_extractor --samtools_path /hpc/hub_oudenaarden/bdebarbanson/bin/samtools-1.3.1 --comprehensive -s --genome_folder /hpc/hub_oudenaarden/avo/BS/mm10 sub_${p}_${smp}
-rm sub_${p}_${smp}
+file="/hpc/hub_oudenaarden/edann/reikVSnla/met_extraction/CpG_context_K562_nla_cat_R2_bismark_bt2.deduplicated.txt.gz"
+output_file="/hpc/hub_oudenaarden/edann/reikVSnla/Nla_lorentz.txt"
+# CpG_context = pd.read_csv(file,  compression='gzip', sep="\t", skiprows=1, usecols=[0], names=["reads"])
 
+# reads=set(list(CpG_context.reads))
+
+
+bismark_dic={}
+with gzip.open(file, "rb") as f:
+	for line in f:
+		aline=line.decode().strip('\n').split('\t')
+		if aline[0] not in bismark_dic.keys():
+			bismark_dic[aline[0]]=[]
+		bismark_dic[aline[0]].append('.'.join(aline[2:4]))
+	f.close()
+
+reads=list(bismark_dic.keys())
+n=len(bismark_dic.keys())
+subsmp_dic={}
+for p in np.arange(1,4000000, 50000):
+	counts=collections.Counter()
+	sample = random.sample(reads, p)
+	for i in sample:
+		for k in bismark_dic[i]:
+			counts[k]+=1
+	subsmp_dic[str(p)]=len(counts.keys())
+
+with open(output_file,"w") as output:
+	for key,val in subsmp_dic.items():
+		print(key+"\t"+str(val), file=output)
