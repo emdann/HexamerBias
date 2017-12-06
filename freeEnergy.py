3be5472a016c8d956703c50723d14388d20187f6
EmmaDann <e.dann@students.uu.nl>
2017-12-06 17:54:53 +0100

    commit message

--- freeEnergy.py
+++ freeEnergy.py
@@ -11,7 +11,7 @@ import pickle
 
 ## rows= first nucleotide (AGTC) cols=second nucleotide (AGTC)
 
-rand_hex=[''.join(i) for i in list(it.product(list("ATCG"),repeat=9))]
+rand_hex=[''.join(i) for i in list(it.product(list("ATCG"),repeat=6))]
 
 def compute_deltaG(hex):
 	deltaG_tbl=np.array([-1.28,-1.54,-1.12,-1.72,-1.58,-2.07,-1.72,-2.53, -0.85, -1.73,-1.28,-1.58, -1.74,-2.49,-1.54,-2.07]).reshape((4,4))
@@ -21,6 +21,7 @@ def compute_deltaG(hex):
 	for i in list(range(len(hex)-1)):
 		#print(deltaG_tbl[pos[hex[i]], pos[hex[i+1]]])
 		hex_G += deltaG_tbl[pos[hex[i]], pos[hex[i+1]]]
+	hex_G += deltaG_tbl[pos[hex[-1]],].mean()
 	return(hex_G)
 
 #rand_hex_deltaG={}
