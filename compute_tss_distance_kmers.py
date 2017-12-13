4e8046ffc078003938aa43d1b9a5f87e3f9aa7b5
EmmaDann <e.dann@students.uu.nl>
2017-12-13 15:48:59 +0100

    commit message

--- compute_tss_distance_kmers.py
+++ compute_tss_distance_kmers.py
@@ -20,87 +20,89 @@ argparser.add_argument('refgen', type=str, help='RefGen file of chr of interest 
 argparser.add_argument('-k', type=int, default=6, required=False, help='Kmer size')
 args = argparser.parse_args()
 
-def kmer_distTSS(params):
-	# compute the distance from a given tss for each hexamer in the region flanking the tss
-	# seq = sequence of the flanking region
-	# 
-	seq,k = params
-	compl_seq=str(Seq(seq, generic_dna).reverse_complement())
+
+def convert_seq(seq):
 	spl_seq=list(seq)
-	# bases=[[spl_seq[i],'T'] if i in list(df[(df.pos==i+1) & (df.strand=='+')].pos-1) else [spl_seq[i],'A'] if i in list(df[(df.pos==i+1) & (df.strand=='-')].pos-1) else spl_seq[i] for i in list(range(len(spl_seq)))]	
-	basesPlus=[[spl_seq[i],'T'] if ((spl_seq[i]=="C") & (spl_seq[i+1]=="G")) else spl_seq[i] for i in range(len(spl_seq)-1)]
-	basesPlus.append(spl_seq[-1])
-	basesPlus=['T' if i=='C' else i for i in basesPlus]
-	spl_seq_compl=list(compl_seq)
-	basesMinus=[[spl_seq_compl[i],'T'] if ((spl_seq_compl[i]=="C") & (spl_seq[i+1]=="G")) else spl_seq_compl[i] for i in range(len(spl_seq_compl)-1)]
-	basesMinus.append(spl_seq_compl[-1])
-	basesMinus=['T' if i=='C' else i for i in basesMinus]
-	tss=(len(seq)/2)-1
+	bases=[[spl_seq[i],'T'] if ((spl_seq[i]=="C") & (spl_seq[i+1]=="G")) else spl_seq[i] for i in range(len(spl_seq)-1)]
+	bases.append(spl_seq[-1])
+	bases=['T' if i=='C' else i for i in bases]
+	return(bases)
+
+def kmer_pos(bases,k):
+	tss=(len(bases)/2)-1
 	tss_dist={}
-	for i in range(0, len(basesPlus)-k+1):
-	    hex = basesPlus[i:i+k]
+	for i in range(0, len(bases)-k+1):
+	    hex = bases[i:i+k]
 	    hex_perm = list(it.product(*hex))
 	    for x in range(len(hex_perm)):
 	    	if(''.join(hex_perm[x]) not in tss_dist.keys()):
 	    		tss_dist[''.join(hex_perm[x])]=[]
+	    	# print ''.join(hex_perm[x])
 	    	tss_dist[''.join(hex_perm[x])].append(i-tss)
-	tss_distMinus={}
-	for i in range(0, len(basesMinus)-k+1):
-	    hex = basesMinus[i:i+k]
-	    hex_perm = list(it.product(*hex))
-	    for x in range(len(hex_perm)):
-	    	if(''.join(hex_perm[x]) not in tss_distMinus.keys()):
-	    		tss_distMinus[''.join(hex_perm[x])]=[]
-		    	tss_distMinus[''.join(hex_perm[x])].append(i-tss)	
-	for key,val in tss_distMinus.items():
-		if key not in tss_dist.keys():
-			tss_dist[key]=[]
-		for pos in val:
-			tss_dist[key].append(pos)
 	return(tss_dist)
+
+def kmer_distTSS(params, conv=True):
+	# compute the distance from a given tss for each hexamer in the region flanking the tss
+	# seq = sequence of the flanking region
+	# 
+	seq,k = params
+	plStrand=seq
+	minStrand=str(Seq(seq, generic_dna).reverse_complement())
+	if conv:
+		basesPlus = convert_seq(plStrand)
+		basesMinus = convert_seq(minStrand)
+	else:
+		basesPlus=list(plStrand)
+		basesMinus=list(minStrand)
+	tss_distPlus = kmer_pos(basesPlus,k)
+	tss_distMinus = kmer_pos(basesMinus,k)	
+	for key,val in tss_distMinus.items():
+		if key not in tss_distPlus.keys():
+			tss_distPlus[key]=[]
+		for pos in val:
+			tss_distPlus[key].append(pos)
+	return(tss_distPlus)
 
 def make_occurrencies_tbl(tss_dist):
 	dic_oc={}
 	hexs=list(tss_dist.keys())
 	counts=[collections.Counter(i) for i in tss_dist.values()]
-	# col_ind = [i for ids in tss_dist.values() for i in ids]
-	# col_ind = list(set(col_ind))
 	for i in list(range(len(hexs))):
 		dic_oc[hexs[i]]=counts[i]
 	oc_tbl=pd.DataFrame(dic_oc).T
 	oc_tbl=oc_tbl.fillna(0)
 	return(oc_tbl)
 
-# chromosome=args.fasta.split('/')[-1].split('.')[0]
-# refgen = pd.read_csv(args.refgen, sep="\t", usecols=[0,2,3,4], header=0, dtype={4:int}) 
-# refgen = refgen[refgen.chrom==chromosome]
-# refgen = refgen.drop_duplicates(subset=None, keep='first', inplace=False)
-# tss = refgen.txStart
-
-refgen=pd.read_csv(args.refgen, sep="\t", header=False, dtype={1:int,2:int}, names=["chrom", "start", "end"])
+chromosome=args.fasta.split('/')[-1].split('.')[0]
+refgen = pd.read_csv(args.refgen, sep="\t", usecols=[0,2,3,4], header=0, dtype={4:int}) 
 refgen = refgen[refgen.chrom==chromosome]
+refgen = refgen.drop_duplicates(subset=None, keep='first', inplace=False)
+tss = refgen.txStart
+
+# refgen=pd.read_csv(args.refgen, sep="\t", header=False, dtype={1:int,2:int}, names=["chrom", "start", "end"])
+# refgen = refgen[refgen.chrom==chromosome]
 
 with ps.FastxFile(args.fasta) as chr:
  	for entry in chr:
  		seq=entry.sequence.upper()
 
-seqs=[]
-for i in range(len(refgen)):
-	start_pos=list(refgen.start)[i]
-	end_pos=list(refgen.end)[i]
-	small_seq=seq[start_pos:end_pos]
-	seqs.append(small_seq)
-
-# flank_wid=6000
 # seqs=[]
-# for i in (tss-1):
-# 	start_pos=i-flank_wid
-# 	end_pos=i+flank_wid
+# for i in range(len(refgen)):
+# 	start_pos=list(refgen.start)[i]
+# 	end_pos=list(refgen.end)[i]
 # 	small_seq=seq[start_pos:end_pos]
 # 	seqs.append(small_seq)
 
+flank_wid=3000
+seqs=[]
+for i in (tss-1):
+	start_pos=i-flank_wid
+	end_pos=i+flank_wid
+	small_seq=seq[start_pos:end_pos]
+	seqs.append(small_seq)
 
-workers = multiprocessing.Pool(10)
+
+workers = multiprocessing.Pool(8)
 tss_dist={}
 for dist in workers.imap_unordered(kmer_distTSS, [ (seqs[i],args.k) for i in list(range(len(seqs)))]):
 	for key,val in dist.items():
