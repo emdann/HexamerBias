2c81b5f8c2fdea4c12483ea8960be12bca5c14a1
EmmaDann <e.dann@students.uu.nl>
2017-12-14 10:16:11 +0100

    commit message

--- compute_tss_distance_kmers.py
+++ compute_tss_distance_kmers.py
@@ -74,10 +74,11 @@ def make_occurrencies_tbl(tss_dist):
 	return(oc_tbl)
 
 chromosome=args.fasta.split('/')[-1].split('.')[0]
-refgen = pd.read_csv(args.refgen, sep="\t", usecols=[0,2,3,4,5], header=0, dtype={4:int}) 
+# refgen = pd.read_csv(args.refgen, sep="\t", usecols=[0,2,3,4,5], header=0, dtype={4:int}) 
+refgen = pd.read_csv(args.refgen, sep="\t", header=None, dtype={4:int}, ames=["chrom", "start", "end"]) 
 refgen = refgen[refgen.chrom==chromosome]
 refgen = refgen.drop_duplicates(subset=None, keep='first', inplace=False)
-tss = refgen.txEnd
+tss = refgen.start
 
 # refgen=pd.read_csv(args.refgen, sep="\t", header=False, dtype={1:int,2:int}, names=["chrom", "start", "end"])
 # refgen = refgen[refgen.chrom==chromosome]
