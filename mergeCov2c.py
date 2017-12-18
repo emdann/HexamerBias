44e5e447b7e2801728aed6470d9ab13d2d690907
EmmaDann <e.dann@students.uu.nl>
2017-12-18 22:30:51 +0100

    commit message

--- mergeCov2c.py
+++ mergeCov2c.py
@@ -10,7 +10,7 @@ argparser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHel
 argparser.add_argument('chrom', type=str, help='chromosome cytosine report input')
 args = argparser.parse_args()
 
-# chrom=args.chrom
+chrom=args.chrom
 dir='/hpc/hub_oudenaarden/edann/hexamers/kaester/met_extraction'
 # dir="/hpc/hub_oudenaarden/edann/reikVSnla/met_extraction"
 files=[]
@@ -31,14 +31,14 @@ with open(files[0], "rb") as f:
 for file in files[1:]:
 	with gzip.open(file,'rb') as f:
 		for line in f:
-			# if chrom+'\\t' in str(line):
-			aline=line.decode().strip('\n').split('\t')
-			if '\t'.join([aline[i] for i in [0,1,2,5,6]]) not in cov_dict.keys():
-				cov_dict['\t'.join([aline[i] for i in [0,1,2,5,6]])]=[0,0]
-			cov_dict['\t'.join([aline[i] for i in [0,1,2,5,6]])][0]+= int(aline[3])
-			cov_dict['\t'.join([aline[i] for i in [0,1,2,5,6]])][1]+= int(aline[4])
+			if chrom+'\\t' in str(line):
+				aline=line.decode().strip('\n').split('\t')
+				if '\t'.join([aline[i] for i in [0,1,2,5,6]]) not in cov_dict.keys():
+					cov_dict['\t'.join([aline[i] for i in [0,1,2,5,6]])]=[0,0]
+				cov_dict['\t'.join([aline[i] for i in [0,1,2,5,6]])][0]+= int(aline[3])
+				cov_dict['\t'.join([aline[i] for i in [0,1,2,5,6]])][1]+= int(aline[4])
 
-output_file=dir+'/merged_reference_CX.cov2c'#+chrom
+output_file=dir+'/merged_reference_CX.cov2c.'+chrom
 with open(output_file, "w") as output:
 	for key,val in cov_dict.items():
 		list_key=key.split("\t")
