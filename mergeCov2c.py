db36ab28e492c3c08f6e7b133f2415de1dafbc8f
EmmaDann <e.dann@students.uu.nl>
2017-12-18 21:41:27 +0100

    commit message

--- mergeCov2c.py
+++ mergeCov2c.py
@@ -10,38 +10,38 @@ argparser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHel
 argparser.add_argument('chrom', type=str, help='chromosome cytosine report input')
 args = argparser.parse_args()
 
-chrom=args.chrom
+# chrom=args.chrom
 dir='/hpc/hub_oudenaarden/edann/hexamers/kaester/met_extraction'
 # dir="/hpc/hub_oudenaarden/edann/reikVSnla/met_extraction"
 files=[]
 for file in os.listdir(dir):
-    if fnmatch.fnmatch(file, '*bismark.cov.gz'):
+    if fnmatch.fnmatch(file, 'cov2c*bismark.cov.gz'):
         files.append(dir+"/"+file)
 
 cov_dict=collections.OrderedDict()
 h=0
-with gzip.open(files[0], "rb") as f:
+with open(files[0], "rb") as f:
 	for line in f:
 		# print("processing line", h)
 		# h+=1
-		if chrom+'\\t' in str(line):
-			aline=line.decode().strip('\n').split('\t')
-			cov_dict['\t'.join([aline[i] for i in [0,1,2]])]=[int(i) for i in aline[4:]]
+		# if chrom+'\\t' in str(line):
+		aline=line.decode().strip('\n').split('\t')
+		cov_dict['\t'.join([aline[i] for i in [0,1,2,5,6]])]=[int(i) for i in aline[3:5]]
 	
 for file in files[1:]:
 	with gzip.open(file,'rb') as f:
 		for line in f:
-			if chrom+'\\t' in str(line):
-				aline=line.decode().strip('\n').split('\t')
-				if '\t'.join([aline[i] for i in [0,1,2]]) not in cov_dict.keys():
-					cov_dict['\t'.join([aline[i] for i in [0,1,2]])]=[0,0]
-				cov_dict['\t'.join([aline[i] for i in [0,1,2]])][0]+= int(aline[4])
-				cov_dict['\t'.join([aline[i] for i in [0,1,2]])][1]+= int(aline[5])
+			# if chrom+'\\t' in str(line):
+			aline=line.decode().strip('\n').split('\t')
+			if '\t'.join([aline[i] for i in [0,1,2,5,6]]) not in cov_dict.keys():
+				cov_dict['\t'.join([aline[i] for i in [0,1,2,5,6]])]=[0,0]
+			cov_dict['\t'.join([aline[i] for i in [0,1,2,5,6]])][0]+= int(aline[3])
+			cov_dict['\t'.join([aline[i] for i in [0,1,2,5,6]])][1]+= int(aline[4])
 
-output_file=dir+'/merged_reference_CX.cov.'+chrom
+output_file=dir+'/merged_reference_CX.cov2c'#+chrom
 with open(output_file, "w") as output:
 	for key,val in cov_dict.items():
 		list_key=key.split("\t")
-		newLine =  list_key[:3]+[str(i/2) for i in val]+list_key[3:]
+		newLine =  list_key[:3]+[str(i) for i in val]+list_key[4:]
 		print('\t'.join(newLine), file=output)
 
