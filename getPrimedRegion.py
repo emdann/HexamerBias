baedb61cb251dc447d48102ec4afea1afc25a777
EmmaDann <e.dann@students.uu.nl>
2018-01-09 11:20:36 +0100

    commit message

--- getPrimedRegion.py
+++ getPrimedRegion.py
@@ -7,8 +7,8 @@ from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.Alphabet import generic_dna
 
-argparser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, description="Count hexamers in BS converted fasta file.\n Do it per chromosome! By Emma Dann")
-argparser.add_argument('bam', type=str, help='Fasta input')
+argparser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, description="Extract position of primer placement from trimmed section of aligned reads. By Emma Dann")
+argparser.add_argument('bam', type=str, help='Input bam file')
 args = argparser.parse_args()
 
 bamfile=args.bam
@@ -20,9 +20,9 @@ bed=[]
 with ps.AlignmentFile(bamfile,"rb") as bam:
 	for r in bam.fetch(until_eof=True):
 		if r.is_read1:
-			bed.append((r.reference_name, r.pos - trim_r1, r.pos - trim_r1 + 6, r.qname))
+			bed.append((r.reference_name, r.pos - trim_r1, r.pos - trim_r1 + 6, r.qname, 1))
 		if r.is_read2:
-			bed.append((r.reference_name, r.pos - trim_r2, r.pos - trim_r2 + 6, r.qname))
+			bed.append((r.reference_name, r.pos - trim_r2, r.pos - trim_r2 + 6, r.qname, 2))
 
 for line in bed:
 	print('\t'.join([str(i) for i in line]))
