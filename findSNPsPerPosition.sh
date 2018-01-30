6833cca4a9e1d796f34daea6331b93e1aa88bfac
Emma <user@Changes-MacBook-Pro.local>
2018-01-30 18:45:44 +0100

    commit message

--- findSNPsPerPosition.sh
+++ findSNPsPerPosition.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
 
 #  findSNPsPerPosition.sh
-#  
+#
 #
 #  Created by Emma on 10/01/2018.
-#  
+#
+if [ $# -ne 2 ]
+then
+    echo "Please, give:"
+    echo "1) input file (.bam,.sam)"
+    echo "2) ref genome (path)"
+    exit
+fi
+
 path2samtools=/hpc/hub_oudenaarden/bdebarbanson/bin/samtools-1.3.1
 
+bam=$1
+refgen=$2
+name=$(echo $bam | sed 's/..am$//')
 
-${path2samtools}/samtools mpileup -O -I -f /hpc/hub_oudenaarden/edann/genomes/mm10/mm10.fa sorted_L1_trim1_R1_bismark_bt2_pe.deduplicated.bam > L1_trim1_R1_bismark_bt2_pe.mpileup
+${path2samtools}/samtools mpileup -O -I -f $refgen $bam > $name.mpileup
 
-cat L1_trim1_R1_bismark_bt2_pe.mpileup |
+cat $name.mpileup |
 awk '$5 ~ /[ACGTNacgtn]/ {print}' | # Select lines with mismatches
 awk '!($3 ~ /[cC]/ && $5=="t" || $5=="T")' | # Rule out lines with C to T conversion
 awk '!($3 ~ /[gG]/ && $5=="a" || $5=="A")' | # Rule out lines with G to A conversion
@@ -30,4 +41,4 @@ awk '
 tr ' ' '\t' |
 awk '$5 ~ /[ACGTNacgtn]/ {print}' |
 awk '!($3 ~ /[cC]/ && $5=="t" || $5=="T")' |
-awk '!($3 ~ /[gG]/ && $5=="a" || $5=="A")' > L1_trim1_R1_bismark_bt2_pe.filt.pileup
+awk '!($3 ~ /[gG]/ && $5=="a" || $5=="A")' > $name.pileup
