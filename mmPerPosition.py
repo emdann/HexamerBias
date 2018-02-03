a7a2b57186dfd8b0d259b2e4bdff383c193b42d1
Emma <user@Changes-MBP.fritz.box>
2018-02-03 15:35:32 +0100

    commit message

--- mmPerPosition.py
+++ mmPerPosition.py
@@ -8,7 +8,6 @@ from hexVSprimed import *
 bamfile='/hpc/hub_oudenaarden/aalemany/emma-adi/gk2a-2.sam'
 
 with ps.AlignmentFile(bamfile,"rb") as bam:
-    countedHex = countHex(bam)
-    df = pd.DataFrame([countedHex]).T
-    df.columns=['usage']
-    df.to_csv('/hpc/hub_oudenaarden/edann/hexamers/rnaseq/hexUsage.csv')
+    mmDic = mmPerPosition(bam)
+    df=make_mm_table(mmDic)
+    df.to_csv('/hpc/hub_oudenaarden/edann/hexamers/rnaseq/mmPerPosition.csv')
