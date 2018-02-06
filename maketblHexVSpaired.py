4697282b4a31e24f7985175c5484db6b4d3bb25f
Emma <user@Changes-MacBook-Pro.local>
2018-02-06 16:25:54 +0100

    commit message

--- maketblHexVSpaired.py
+++ maketblHexVSpaired.py
@@ -5,14 +5,18 @@ import argparse
 import pandas as pd
 
 argparser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, description="Get hexamers used in fasta file.\n By Emma Dann")
-argparser.add_argument('fasta', type=str, help='Fasta input')
+argparser.add_argument('bam', type=str, help='Bam input')
 argparser.add_argument('primedreg', type=str, help='Fasta input')
+argparser.add_argument('ref', type=str, help='template or primer')
 args = argparser.parse_args()
 
 fasta = args.primedseq_fasta
+bamfile=args.bam
+ref = args.ref
 
-bamfile='/hpc/hub_oudenaarden/aalemany/emma-adi/zebrafish/gk2a-2.sam'
-fasta='/hpc/hub_oudenaarden/edann/hexamers/rnaseq/gk2a-2_primed_seq.fa'
+
+# bamfile='/hpc/hub_oudenaarden/aalemany/emma-adi/mouse/SvdB11d3-MitoTrackerThird-Satellites-Adult.sam'
+# fasta='/hpc/hub_oudenaarden/edann/hexamers/rnaseq/SvdB11d3-MitoTrackerThird-Satellites-Adult.sam_primed_reg.qname.bed.fa'
 
 templDic={}
 with ps.FastxFile(fasta) as templ:
@@ -20,30 +24,39 @@ with ps.FastxFile(fasta) as templ:
         seq, name = entry.sequence.upper(), entry.name
         templDic[name]=[seq]
 
-
 with ps.AlignmentFile(bamfile,"rb") as bam:
-    for r in bam.fetch(until_eof=True): 
+    for r in bam.fetch(until_eof=True):
         if r.flag==0:
             templDic[r.qname].append(r.seq[0:6])
         elif r.flag==16:
             templDic.pop(r.qname)
+#
+# SOI=["GTGTGT", "TGTGTG" ,"TTCCCC", "TAGAAC" ,"ACGAAT" ,"TAGGAA" ,
+#     "AAAAAA" ,"TTTTTT", "GGGGGG", "CCCCCC",
+#     "GTAGGT","GTACGC", "GTACCG", "AAAAAT", "ATGAAA",
+#     "CGACGT", "ATTCAC", "TGATTT" ,"CTTTAT" ,"ACGACT", "AACCCT", "GGATGC", "CTGGAT", "CAGCAG", "TCGCAC"]
+#
 
-SOI=["GTGTGT", "TGTGTG" ,"TTCCCC", "TAGAAC" ,"ACGAAT" ,"TAGGAA" ,
-    "AAAAAA" ,"TTTTTT", "GGGGGG", "CCCCCC",
-    "GTAGGT","GTACGC", "GTACCG", "AAAAAT", "ATGAAA",
-    "CGACGT", "ATTCAC", "TGATTT" ,"CTTTAT" ,"ACGACT", "AACCCT", "GGATGC", "CTGGAT", "CAGCAG", "TCGCAC"]
-
-SOICounter = {key:collections.Counter() for key in SOI}
+SOICounter = {}
 for seqs in templDic.values():
     templ,primer=seqs[0],seqs[1]
-    if primer in SOI:
-        SOICounter[primer][templ]+=1
+    if ref=='template':
+        key, val=templ,primer
+    elif ref=='primer':
+        key, val=primer, templ
+    if key not in SOICounter.keys():
+        SOICounter[key]=collections.Counter()
+    SOICounter[key][val]+=1
 
 pwms={k:makePWMtempl(v) for k,v in SOICounter.items()}
-entropies = [computeEntropy(i) for i in pwms]
+entropies = {k:computeEntropy(v) for k,v in pwms.items()}
 
-for k,pwm in pwms.items():
-    pwm.to_csv('/hpc/hub_oudenaarden/edann/hexamers/rnaseq/'+k+'.primerDanRer.pwm.csv')
 
-tbl = pd.DataFrame(SOICounter).fillna(0)
-tbl.to_csv(primedregFile.split("/")[-1].split('.fa')[0] +'.csv')
+print(ref+'\tentropy')
+for k,v in entropies.items():
+    
+# for k,pwm in pwms.items():
+#     pwm.to_csv('/hpc/hub_oudenaarden/edann/hexamers/rnaseq/'+k+'.primerDanRer.pwm.csv')
+
+# tbl = pd.DataFrame(SOICounter).fillna(0)
+# tbl.to_csv(primedregFile.split("/")[-1].split('.fa')[0] +'.csv')
