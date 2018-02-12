2d800d393574be83f389889b66c7bbc1e368bf8d
Emma <user@Changes-MacBook-Pro.local>
2018-02-12 15:25:32 +0100

    com

---
+++ predictCoverage.py
@@ -0,0 +1,111 @@
+import fnmatch
+import os
+import pandas as pd
+import numpy as np
+import multiprocessing
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+argparser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, description="Get hexamers used in fasta file.\n By Emma Dann")
+argparser.add_argument('predDg', type=str, help='Matrix of predicted Dg')
+argparser.add_argument('cellPtCount', type=str, help='Matrix of pt occurrencies in a cell')
+argparser.add_argument('-t', type=int, default=0, required=False, help='Threshold of pt events to predict Dg on')
+args = argparser.parse_args()
+
+def predictCov(t,DgRow):
+    '''
+    Computes the predicted coverage for random hexamer experiment.
+    '''
+    sumRow = sum(np.exp(DgRow).fillna(0))
+    cov = t*sumRow
+    return(cov)
+
+def makePredictedDgMatrix(file, cellAb):
+
+    '''
+    Turns file of pt - predicted Dg (precessed in R) to matrix of template on row and primer on column
+    Needs cell abundance file to fill in missing pt pairs
+    '''
+    df = pd.read_csv(file, index_col=0)
+    tabDic={}
+    for row in df.iterrows():
+        pt,dg = row[1].pt, row[1].predictedDg
+        t,p = pt.split('-')
+        if t not in tabDic.keys():
+            tabDic[t]={}
+        tabDic[t][p]=dg
+    ptMat = pd.DataFrame(tabDic)
+    for temp in [i for i in cellAb.index if i not in ptMat.index]:
+        newRow=pd.DataFrame(np.nan, index=[temp], columns=ptMat.columns)
+        ptMat = ptMat.append(newRow)
+    for primer in [i for i in cellAb.index if i not in ptMat.columns]:
+        newCol=pd.DataFrame(np.nan, index=[primer], columns=ptMat.index).T
+        ptMat = pd.concat([ptMat, newCol], axis=1)
+    ptMat=ptMat.sort_index(axis=1).sort_index()
+    return(ptMat)
+
+def setThresh4Dg(dgMat,ptMat,thresh=1):
+    '''
+    Assuming that if a pt interaction is seen only once then it is an error, the predicted Dg value
+    for a pt couple with conc = 1 is set to -99999
+    '''
+    dgMat[ptMat <= thresh ] = -99999.0
+    return(dgMat)
+
+def findCompr(filename):
+    if filename.endswith('gz'):
+        compr='gzip'
+    else:
+        compr='infer'
+    return(compr)
+
+# # Make predicted Dg tab
+# file = 'predictedDg_over20kreads.csv'
+# df = makePredictedDgMatrix('predictedDg_over20kreads.csv', cellAb)
+# df.to_csv(path+'gk2a-2_predictedDg_over20kreads.csv')
+
+path='/hpc/hub_oudenaarden/edann/hexamers/rnaseq/'
+
+predictedDg = args.predDg
+ptMatrix = args.cellPtCount
+cellAbundanceTab = 'gk2a-2.cellAbundance.noN.csv'
+thresh=args.t
+
+
+cell = ptMatrix.split('pt')[0][4:]
+
+tab = pd.read_csv(path+predictedDg, index_col=0, compression=findCompr(predictedDg))
+if thresh:
+    tab=setThresh4Dg(tab,thresh=thresh)
+
+tabAb=pd.read_csv(path+cellAbundanceTab, index_col=0, compr=findCompr(cellAbundanceTab))
+cellAb = tabAb[cell]
+
+ptMat = pd.read_csv(path+ptMatrix, compression = findCompr(ptMatrix), index_col=0)
+ptMat = ptMat[[i for i in ptMat.columns if 'N' not in i]]
+for temp in [i for i in cellAb.index if i not in ptMat.index]:
+    newRow=pd.DataFrame(0, index=[temp], columns=ptMat.columns)
+    ptMat = ptMat.append(newRow)
+
+for primer in [i for i in cellAb.index if i not in ptMat.columns]:
+    newCol=pd.DataFrame(0.0, index=[primer], columns=ptMat.index).T
+    ptMat = pd.concat([ptMat, newCol], axis=1)
+
+ptMat=ptMat.sort_index(axis=1).sort_index()
+
+list=[]
+with open(path+'predicetdCov/gk2a-2.CovPred.'+cell+'.thresh'+thresh+'.txt', 'w') as output:
+    print('template','obs', 'exp', sep='\t', file=output)
+    for templ in tab.iterrows():
+        t,DgRow = templ
+        # list.append((sum(ptMat[t]), predictCov(cellAb[t],DgRow)))
+        print(t, sum(ptMat[t]), predictCov(cellAb[t],DgRow), sep='\t', file=output)
+# obs = [x for x,y in list]
+# exp = [y for x,y in list]
+#
+# plt.figure(1)
+# plt.plot(obs,exp, 'b.')
+# plt.xlabel('observed coverage')
+# plt.ylabel('predicted coverage')
+# plt.savefig('/hpc/hub_oudenaarden/edann/output/test_obsVSexp_cov_cell194.pdf')
--- rscripts/correlationDgPrediction.r
+++ rscripts/correlationDgPrediction.r
@@ -1,8 +1,11 @@
 library(dplyr)
 library(data.table)
 library(ggplot2)
+library(RColorBrewer)
+library(nortest)
+library(pheatmap)
 
-ptPairs <- fread('mnt/edann/hexamers/rnaseq/gk2a-2.ptPairs.csv', header=TRUE, sep=',')
+ptPairs <- fread('mnt/edann/hexamers/rnaseq/commonPtPairs_allCells_adj.csv', header=TRUE, sep=',')
 colnames(ptPairs) <- c('pt', paste0('cell',colnames(ptPairs[,-1])))
 
 # Correlation between cells
@@ -14,14 +17,32 @@ p1 %>%  ggplot(.,aes(cell363,cell121, color=GCcontPrimer)) + geom_point() +
   ggsave('AvOwork/output/deltaGprediction/exCor_gcPrimer.pdf')
 
 corr <- cor(ptPairs[,-1],use = 'pairwise.complete.obs')
-pdf('AvOwork/output/deltaGprediction/PCC_hist_gk2a.pdf')
+pdf('AvOwork/output/deltaGprediction/PCC_hist_gk2a_allCells.pdf')
 hist(corr, breaks = 15, xlim=c(0,1), xlab='PCC', main = 'Correlation between predicted DeltaG values in single cells')
 dev.off()
+
+no.reads <- read.delim('mnt/edann/hexamers/rnaseq/gk2a-2_alignedReadsPerCells.txt', header=F, sep=' ')
+anno.row <- data.frame(row.names =paste0('cell',no.reads[match(rownames(corr), paste0('cell',no.reads$V2)),]$V2), no.reads=no.reads[match(rownames(corr), paste0('cell',no.reads$V2)),]$V1)
+anno = data.frame(row.names = rownames(anno.row), alignedReads=ifelse(anno.row$no.reads<20000, '< 20000', '> 20000'))
+my.breaks <- c(10000,20000,30000,max(anno.row$no.reads))
+ii <- cut(anno.row$no.reads, breaks = my.breaks, include.lowest = TRUE)
+colors <- brewer.pal(3, 'RdBu')[ii]
+p <- pheatmap(corr, show_rownames = F, show_colnames = F,
+         legend_breaks = c(0.5, 0.6, 0.7, 0.8, 0.9, max(corr)), legend_labels=c(0.5, 0.6, 0.7, 0.8, 0.9, 'PCC'),
+         cellheight=2.5,cellwidth=2.5, 
+         border_color = NA,annotation_row = anno, annotation_col = anno, annotation_names_row = FALSE, annotation_names_col = FALSE,# annotation_colors = list(no.reads=colors),
+         annotation_legend = TRUE, fontsize = 12,
+         filename = 'AvOwork/output/deltaGprediction/cellsCorr_heatmap_withAnno.pdf', height = 10, width = 10)
+
+corClust <- names(which(cutree(p$tree_col, k=2)==1))
 
 # Distribution of values for couple
 ptOrdGcTempl <- ptPairs %>% mutate(gccontTempl=sapply(substr(ptPairs$pt, 1, 6), GCcont)) %>% arrange(gccontTempl)
 ptDiag <- ptOrdGcTempl %>% filter(substr(pt,1,6)==substr(pt,8,13))
-diagIx <- which(ptPairs$pt %in% ptDiag$pt)
+diagIx <- which(ptOrdGcTempl$pt %in% ptDiag$pt)
+
+complRows <- which(complete.cases(ptOrdGcTempl))
+GCsortedRows <- complRows[order(sapply(substr(ptPairs[complRows,]$pt,1,6), GCcont))]
 
 sampleIxFromBins <- function(ptOI){
   binnedPt <- split(ptOI, ceiling(seq_along(ptOI)/100))
@@ -29,19 +50,46 @@ sampleIxFromBins <- function(ptOI){
   return(smp)
   }
 
-boxplotPtDg <- function(ptOI, GCord=TRUE){
+boxplotPtDg <- function(ptOI, ptTab=ptOrdGcTempl, GCord=TRUE){
   if(GCord==TRUE){
-    print('noice')
-    ptTab <- ptOrdGcTempl
+    # print('noice')
+    ptTab <- ptTab
   }else{
     ptTab <- ptPairs
-    print('not noice')
+    # print('not noice')
   }
-  par(mar = c(5, 7, 4, 2))
+  ## make color scale
+  values=ptTab[ptOI,]$gccontTempl
+  ii <- cut(values, breaks = seq(0, 1, len = 100), include.lowest = TRUE)
+  colors <- colorRampPalette(c('lightblue','white','firebrick'))(99)[ii]
+  par(mar = c(5, 7, 4, 2), xpd=TRUE)
   boxplot(t(sapply(ptTab[ptOI,-1], as.numeric)), 
-          names=ptTab[ptOI]$pt,las=2, cex.names=0.4, horizontal=TRUE, cex.axis=0.7,
-          xlab='predicted DeltaG', mar=c(1,2,3,10), ylim=c(-11,-2))
-  } 
+          names=ptTab[ptOI,]$pt,las=2, cex.names=0.4, horizontal=TRUE, cex.axis=0.7, col=colors, 
+          xlab='predicted DeltaG', mar=c(1,2,3,10), ylim=c(-11,-1))
+  legend("topleft", inset=c(0.4,-0.05), legend=c("> AT","> GC"), fill = c('lightblue', 'firebrick'), horiz = TRUE, bty = 'n')
+} 
 
-complRows <- which(complete.cases(ptPairs))
-GCsortedRows <- complRows[order(sapply(substr(ptPairs[complRows,]$pt,1,6), GCcont))]
+rowHist <- function(row, ptOrdGcTempl){
+  y=as.numeric(ptOrdGcTempl[row,-ncol(ptOrdGcTempl)])
+  par(mfrow=c(1,2), mar=c(5, 4, 4, 2) - 0.1)
+  hist(y, xlab='predictedDg', breaks=15, main = ptOrdGcTempl[row,1], xlim=c(-1,-12))
+  abline(v = mean(y, na.rm = T), col='red', xpd=F)
+  legend('top', 'mean', lty=1, col = 'red', bty='n', xpd=T, inset = c(0,-0.05))
+  qqnorm(y)
+  qqline(y, xpd=F)
+  test = shapiro.test(y)
+  legend('topleft', paste0('SW test p-val = ', round(test$p.value, digits = 4)), cex = 0.7, bty='n')
+  }
+
+pdf('AvOwork/output/deltaGprediction/complRows_DgDistribution.pdf', onefile = T, width = 10 )
+sapply(complRows, rowHist, ptOrdGcTempl=ptOrdGcTempl)
+dev.off()
+pdf('AvOwork/output/deltaGprediction/complRows_DgDistribution_goodCluster.pdf', onefile = T, width = 10 )
+sapply(complRows, rowHist, ptOrdGcTempl=ptOrdGcTempl[,c('pt',corClust)])
+dev.off()
+# Predicted deltaG
+ptWithDg <- ptOrdGcTempl %>% mutate(predictedDg = apply(ptOrdGcTempl[,rownames(anno.row>20000)],1, median, na.rm=TRUE)) 
+write.csv(ptWithDg[c('pt', 'predictedDg')])
+
+dgMatrix <- matrix()
+
