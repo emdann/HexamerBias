a8512313288e83c27b0142a1b513452ff1dcf5e0
Emma <user@Changes-MacBook-Pro.local>
2018-03-02 13:43:11 +0100

    ds

--- snakemake/cellPrimerTemplTab.py
+++ snakemake/cellPrimerTemplTab.py
@@ -54,7 +54,7 @@ def split_pt_dic(templDic):
     return(cellDic)
 
 def save_ptCounts(cellDic,cellsOI,fasta, cores=10):
-    abundanceFile = fasta.strip('.primedreg.fa')+'.cellAbundance.csv'
+    abundanceFile = fasta.strip('.primedreg.fa')+'.cellAbundance.noN.csv'
     tabAb = pd.read_csv(abundanceFile, index_col=0)
     path = '/'.join(fasta.split('/')[:-1])
     if path:
