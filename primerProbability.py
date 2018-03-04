617daf2824ca6bc84e0f90ca90d623faf34a764e
Emma <user@Changes-MacBook-Pro.local>
2018-03-04 18:10:53 +0100

    primerprob

--- primerProbability.py
+++ primerProbability.py
@@ -1,1 +1,12 @@
-### 
+import numpy as np
+import pandas as pd
+'''
+Compute the probability for all possible hexamers base on manufacturers concentrations.
+Input: matrix of base concentration per position
+Output: probability for all hexamers
+'''
+
+# Make structured array for concentrations
+x = np.array([[0.25,0.25,0.25, 0.25, 0.25, 0.25],
+            [0.25,0.25,0.25, 0.25, 0.25, 0.25]],
+    dtype=[('foo', 'i4'),('bar', 'f4'), ('baz', 'S10')])
