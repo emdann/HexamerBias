aa2c6a6b6552774479253fe61e338818ffb4044b
Emma <user@Changes-MacBook-Pro.local>
2018-03-20 15:15:19 +0100

    bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -24,7 +24,7 @@ fi
 bin_dir=/hpc/hub_oudenaarden/edann/bin/coverage_bias
 
 echo "--- Step 1: get primed region --- "
-python ${bin_dir}/getPrimedRegion.py -o ./ $bamfile $refgen bs_se
+python ${bin_dir}/getPrimedRegion.py -o ./ $bamfile $refgen bs_pe
 echo "--- Step 2: Make pt table --- "
 python ${bin_dir}/bsPrimerTemplTab.py $fasta ${sample}.primedreg.fa $abfile
 echo "--- Step 3: predict delta G --- "
