94e0606885c9572dff42901f3472298f7c260061
Emma <user@Changes-MacBook-Pro.local>
2018-03-23 13:54:18 +0100

    artificial coverage

--- artificial_coverage/genome_wide_artificialcov.py
+++ artificial_coverage/genome_wide_artificialcov.py
@@ -14,16 +14,17 @@ def from_refgen_to_bed(genomefile):
                 beds.append(chr+' 0 '+length)
     return(beds)
 
-def genome_wide_artificial_coverage(genomefile,covFile,abundanceFile, outfile):
+def genome_wide_artificial_coverage(genomefile,covFile,refgen,abundanceFile, outfile):
     abundance = pd.read_csv(abundanceFile, index_col=0, compression=findCompr(abundanceFile), header=None)
     coverage = pd.read_table(covFile, index_col=0, sep='\t',compression=findCompr(covFile))
     density = template_density(coverage.exp,abundance)
     beds = from_refgen_to_bed(genomefile)
-    covBed = artificial_cov(beds)
+    covBed = artificial_cov(beds,refgen,density)
     save_coverage_bed(covBed, outfile=outfile)
     return(covBed)
 
 genomefile='/hpc/hub_oudenaarden/edann/genomes/mm10/mm10.genome'
 abundanceFile = "/hpc/hub_oudenaarden/edann/hexamers/VAN1667prediction/mm10.cellAbundance.noN.csv.gz"
 covFile = "predictedCoverage_avgVAN1667.txt"
-genome_wide_artificial_coverage(genomefile, covFile, abundanceFile, outfile='mm10.artificial_cov.VAN1667avg.bed')
+refgen='/hpc/hub_oudenaarden/edann/genomes/mm10/mm10.fa'
+genome_wide_artificial_coverage(genomefile, covFile, refgen,abundanceFile, outfile='mm10.artificial_cov.VAN1667avg.bed')
