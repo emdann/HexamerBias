e8aa312ff60f28b8c26d7ce63853b29fb8dc9ead
Emma <user@Changes-MacBook-Pro.local>
2018-03-29 10:51:46 +0200

    DE implementation

--- DEimplementation.py
+++ DEimplementation.py
@@ -16,20 +16,20 @@ def de(fobj, seq_len, mut=0.8, crossp=0.7, popsize=20, its=1000, cores=10):
     best_idx = np.argmin(fitness)   ## Returns the indices of the minimum values along an axis.
     best = pop_denorm[best_idx]
     for i in range(its):
-        for f,j in workers.imap_inordered(de_mutation, [ (fobj,j,popsize,mut,crossp) for j in range(popsize)])
+        for f,trial,j in workers.imap_unordered(de_mutation, [ (fobj,j,pop,seq_len,popsize,mut,crossp) for j in range(popsize)]):
             if f < fitness[j]:
                 fitness[j] = f
                 pop[j] = trial
             if f < fitness[best_idx]:
                 best_idx = j
-                best = trial_denorm
+                best = trial
     yield best, fitness[best_idx]
 
 def de_mutation(params):
     '''
     Function for mutation of one vector in the population of vectors
     '''
-    fobj,j,popsize,mut,crossp = params
+    fobj,j,pop,seq_len,popsize,mut,crossp = params
     idxs = [idx for idx in range(popsize) if idx != j]
     a, b, c = pop[np.random.choice(idxs, 3, replace = False)]
     mutant = np.clip(a + mut*(b - c), 0, 1)
@@ -39,21 +39,14 @@ def de_mutation(params):
     trial = np.where(cross_points, mutant, pop[j])
     trial_denorm = reset_costraints(trial)
     f = fobj(trial_denorm)
-    return(f,j)
+    return(f,trial_denorm,j)
 
-    if f < fitness[j]:
-        fitness[j] = f
-        pop[j] = trial
-    if f < fitness[best_idx]:
-        best_idx = j
-        best = trial_denorm
-
-arr=np.asarray(random_ppm(0.01))
-vec = arr.reshape([1,24])
-
-arr2 = np.random.rand(6,4).reshape(1,24)
-arr2 /= arr2.sum(axis=1)[:,np.newaxis]
-arr2.reshape(1,24)
+# arr=np.asarray(random_ppm(0.01))
+# vec = arr.reshape([1,24])
+#
+# arr2 = np.random.rand(6,4).reshape(1,24)
+# arr2 /= arr2.sum(axis=1)[:,np.newaxis]
+# arr2.reshape(1,24)
 
 def reset_costraints(prob_vector):
     '''
@@ -72,3 +65,5 @@ def test_function(prob):
     '''
     hexProb = prob_from_ppm(from_vec_to_ppm(prob), all_hexamers())
     return(hexProb.loc['CGCGCG'][0])
+
+list(de(test_function, 6, popsize=20, its=1000, cores=10))
