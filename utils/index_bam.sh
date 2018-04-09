3bf276a0704d29a7170edd4e69d5e4bbd4ddcf9c
Emma <user@Changes-MacBook-Pro.local>
2018-04-09 13:28:48 +0200

    sst

--- utils/index_bam.sh
+++ utils/index_bam.sh
@@ -4,5 +4,7 @@ bam=$1
 sample=$(echo $bam | awk '{gsub(/.bam/, ""); print}')
 path2samtools=/hpc/hub_oudenaarden/bdebarbanson/bin/samtools-1.4.1
 
+echo "Sorting file into ${sample}.srt.bam"
 ${path2samtools}/samtools sort -@ 5  ${bam} -o ${sample}.srt.bam
+echo "Indexing file ${sample}.srt.bam"
 ${path2samtools}/samtools index -@ 5 ${sample}.srt.bam
