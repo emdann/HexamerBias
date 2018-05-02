25a40fb25d94a0893f4771ba81f889bd4763dca6
Emma <user@Changes-MacBook-Pro.local>
2018-05-02 16:28:48 +0200

    genome kmers

---
+++ deltaGprediction/kmersInGenome.py
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# < 1 ]
+then
+    echo "Please, give:"
+    echo "1) Fasta file of reference genome"
+    echo "Optional: "
+    echo "2) Number of threads to use (default = 8)"
+    exit
+fi
+
+genome=$1
+threads=${2:-8}
+
+organism=$(echo $refgen | sed 's,.*/,,g' | sed 's/.fa//')
+bin_dir=/hpc/hub_oudenaarden/edann/bin/coverage_bias/utils
+
+source /hpc/hub_oudenaarden/edann/venv2/bin/activate
+python ${bin_dir}/kmerCounter.py -t $threads > ${organism}.kmerAbundance.csv
