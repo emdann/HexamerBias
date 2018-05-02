900c05aa01606b9c456f9bc1d8b8aecfb56df0c4
Emma <user@Changes-MacBook-Pro.local>
2018-05-02 17:13:22 +0200

    genome kmers

--- deltaGprediction/kmersInGenome.sh
+++ deltaGprediction/kmersInGenome.sh
@@ -15,5 +15,6 @@ threads=${2:-8}
 organism=$(echo $refgen | sed 's,.*/,,g' | sed 's/.fa//')
 bin_dir=/hpc/hub_oudenaarden/edann/bin/coverage_bias/utils
 
+echo $organism
 source /hpc/hub_oudenaarden/edann/venv2/bin/activate
-python ${bin_dir}/kmerCounter.py -t $threads $genome | grep -v N > ${organism}.kmerAbundance.csv
+python ${bin_dir}/kmerCounter.py -t $threads $genome | grep -v N > $organism.kmerAbundance.csv
