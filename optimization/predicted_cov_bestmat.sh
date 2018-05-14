43ec06f4dcf6b35de75bf54ef4e7fa9502e30341
Emma <user@Changes-MacBook-Pro.local>
2018-05-14 15:21:27 +0200

    pred cov bestmat

--- optimization/predicted_cov_bestmat.sh
+++ optimization/predicted_cov_bestmat.sh
@@ -16,6 +16,7 @@ venv=/hpc/hub_oudenaarden/edann/venv2/bin/activate
 kmerAbFile=/hpc/hub_oudenaarden/edann/hexamers/genomes_kmers/mm10.kmerAbundance.csv
 genomefa=/hpc/hub_oudenaarden/edann/genomes/mm10/mm10.fa
 
+source $venv
 ${bindir}/optimization/coverage_best_matrix.py $sample
 echo "source $venv; python ${bindir}/artificial_coverage/strand_specific_artificial_coverage.py $kmerAbFile ${sample}.bestMat.coverage.csv $testbed $genomefa -t 10" | \\
   qsub -cwd -N predictedcov_${sample} -pe threaded 10 -l h_rt=24:00:00 -l h_vmem=80G -l h_cpu=10:00:00
