daf03fdbb04b67f2b8184817f70f95642280e544
Emma <user@Changes-MacBook-Pro.local>
2018-05-21 10:01:02 +0200

    fixing index_bam

--- utils/index_bam.sh
+++ utils/index_bam.sh
@@ -4,7 +4,14 @@ bam=$1
 sample=$(echo $bam | awk '{gsub(/.bam/, ""); print}')
 path2samtools=/hpc/hub_oudenaarden/bdebarbanson/bin/samtools-1.4.1
 
-echo "Sorting file into ${sample}.srt.bam"
-${path2samtools}/samtools sort -@ 5  ${bam} -o ${sample}.srt.bam
+if [[ $sort_order =~ 'unsorted' ]] ;
+then
+  echo "Sorting file into ${sample}.srt.bam"
+  ${path2samtools}/samtools sort -@ 5  ${bam} -o ${sample}.srt.bam
+  bam=${sample}.srt.bam
+else
+  echo "Bamfile is sorted"
+fi
+
 echo "Indexing file ${sample}.srt.bam"
-${path2samtools}/samtools index -@ 5 ${sample}.srt.bam
+${path2samtools}/samtools index -@ 5 $bam
