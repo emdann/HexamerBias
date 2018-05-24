3d50206eeeca55a08961633e4067f51ab98e73b2
Emma <user@Changes-MacBook-Pro.local>
2018-05-24 11:49:03 +0200

    misc fix

--- deltaGprediction/getPrimedRegion.py
+++ deltaGprediction/getPrimedRegion.py
@@ -111,22 +111,22 @@ def get_strandspecific_template_bed(bamfile, trim=9, type='bs_se'):
 	Extract positions of template regions for primers of aligned reads in bam (se mapped bismark).
 	For type = 'rna' extracts the first 6 bases of the aligned read
 	'''
-trim=trim
-bed=[]
-if type=='bs_se' or type=='no_bs': # Define flags
-	plus=0
-	minus=16
-if type=='bs_pe':
-	plus=99
-	minus=83
-with ps.AlignmentFile(bamfile,"rb") as bam:
-	for r in bam.fetch(until_eof=True):
-		if not bool(re.search("[^a-zA-Z]", r.qqual[0:6])): # Filter for sequencing quality
-			if r.flag==plus:
-				bed.append((r.reference_name, r.pos - trim, r.pos - trim + 6, r.qname, '.', '+'))
-			if r.flag==minus:
-				bed.append((r.reference_name, r.pos - trim, r.pos - trim + 6, r.qname, '.', '-'))
-return(bed)
+	trim=trim
+	bed=[]
+	if type=='bs_se' or type=='no_bs': # Define flags
+		plus=0
+		minus=16
+	if type=='bs_pe':
+		plus=99
+		minus=83
+	with ps.AlignmentFile(bamfile,"rb") as bam:
+		for r in bam.fetch(until_eof=True):
+			if not bool(re.search("[^a-zA-Z]", r.qqual[0:6])): # Filter for sequencing quality
+				if r.flag==plus:
+					bed.append((r.reference_name, r.pos - trim, r.pos - trim + 6, r.qname, '.', '+'))
+				if r.flag==minus:
+					bed.append((r.reference_name, r.pos - trim, r.pos - trim + 6, r.qname, '.', '-'))
+	return(bed)
 
 def get_strandspecific_template_fasta(bamfile, fi, outpath, type):
 	'''
