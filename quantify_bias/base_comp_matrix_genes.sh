978c1c498e740b6a8cecbc64a23238e9e958b779
Emma <user@Changes-MacBook-Pro.local>
2018-05-28 17:15:19 +0200

    random

--- quantify_bias/base_comp_matrix_genes.sh
+++ quantify_bias/base_comp_matrix_genes.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 
-gen_fasta=/hpc/hub_oudenaarden/gene_models/cel_gene_models/WBcel235.fa
-bed=/hpc/hub_oudenaarden/edann/hexamers/annotations_bed/RefSeq_genes_WBcel235_noChr.bed
-gen_file=/hpc/hub_oudenaarden/edann/genomes/WBcel235.genome
+gen_fasta=/hpc/hub_oudenaarden/gene_models/zebrafish_gene_models/danRer10_clean.fa
+bed=/hpc/hub_oudenaarden/edann/hexamers/annotations_bed/RefSeq_genes_danRer10.bed
+gen_file=/hpc/hub_oudenaarden/edann/genomes/danRer10.genome
 bins=$1
 slop=$2
 
-cat $bed  | cut -f 1,2,3,4,5,6 | grep '+' | bedtools makewindows -b stdin -n $bins -i winnum > genes_plus.bed
-cat $bed  | cut -f 1,2,3,4,5,6 | grep '-' | bedtools makewindows -reverse -b stdin -n $bins -i winnum > genes_minus.bed
+bedtools sample -n 10000 -i $bed > smp_bed.bed
+bed=smp_bed.bed
 
-cat genes_*us.bed | \
+cat $bed  | cut -f 1,2,3,4,5,6 | grep '+' | \
+  bedtools makewindows -b stdin -n $bins -i winnum | \
+  awk '{$4=$4+300; print}' > genes_plus.bed
+cat $bed  | cut -f 1,2,3,4,5,6 | grep '-' | \
+  bedtools makewindows -reverse -b stdin -n $bins -i winnum | \
+  awk '{$4=$4+300; print}' > genes_minus.bed
+
+# Left flanks
+cat $bed  | cut -f 1,2,3,4,5,6 | \
+  bedtools flank -s -i stdin -g $gen_file -l 3000 -r 0| \
+  bedtools makewindows -b stdin -n 300 -i winnum > genes_left_flank.bed
+# Right flanks
+cat $bed  | cut -f 1,2,3,4,5,6 | \
+  bedtools flank -s -i stdin -g $gen_file -l 0 -r 3000| \
+  awk '$1 !~ /_/' | \
+  bedtools makewindows -b stdin -n 300 -i winnum | \
+  awk '{$4=$4+800; print}' > genes_right_flank.bed
+
+cat genes_*.bed | \
+  tr ' ' '\t' | \
   bedtools nuc -fi $gen_fasta -bed stdin | \
+  tail -n+2 | \
   sort -k4n | \
-  bedtools groupby -i stdin -g 4 -c 5,6 -o mean,mean > nuc_refseq_genes_WBcel235.txt
+  bedtools groupby -i stdin -g 4 -c 5,6 -o mean,mean > nuc_refseq_genes_danRer10smp.txt
 
 rm genes_*us.bed
