32e603268ecc50fd6de44524c9cf2851efa28bb8
Emma <user@Changes-MacBook-Pro.local>
2018-07-12 16:14:56 +0200

    save pt tabs

---
+++ deltaGprediction/pt_tab_to_rdata.r
@@ -0,0 +1,19 @@
+#!/usr/bin/Rscript
+source("//hpc/hub_oudenaarden/edann/bin/coverage_bias/deltaGprediction/binding_model_functions.r")
+
+args <- commandArgs(trailingOnly = T) 
+# Make parsing better
+pt.file <- args[1]
+ab.file <- args[2]
+outfile <- args[3]
+
+print("Loading pt file")
+pt.df <- load.pt.data(pt.file)
+genome.abundance <- load.kmer.abundance(ab.file)
+mfold.dg <- load.modelled.deltaG("/hpc/hub_oudenaarden/edann/hexamers/rand_hex_deltaG_ions.txt.gz")
+
+print("Merging")
+pt.all.df <- join.pt.data(pt.df$matches, pt.df$t.usage, genome.abundance, mfold.dg)
+
+print("Saving")
+save(pt.all.df, file = outfile)
