921fcac023f6c879114c7ad9ee4ff9aac52e892d
Emma <user@Changes-MacBook-Pro.local>
2018-07-13 10:36:22 +0200

    downsampling

--- deltaGprediction/epsilon_downsampling.r
+++ deltaGprediction/epsilon_downsampling.r
@@ -5,8 +5,12 @@ args <- commandArgs(trailingOnly = T)
 # Make parsing better
 rds.file <- args[1]
 
+print("Loading pt table")
 pt.df <- readRDS(rds.file)
 pt.diag.df <- filter(pt.df, template==primer)
+
+print("Estimating espilon")
 eps <- epsilon.minimize.chisq(pt.diag.df, max=200000, plot=F)
 
-write(paste(gsub(rds.files[1], pattern='.+/|.RDS', replacement = ''), eps, sep=','), file = 'bootstrap_epsilon.txt', append = T)
+print("Writing result to file")
+write(paste(gsub(rds.file, pattern='.+/|.RDS', replacement = ''), eps, sep=','), file = 'bootstrap_epsilon.txt', append = T)
