756e8aeeb0884c3d3f4577189a2b7927bc753f4f
Emma <user@Changes-MacBook-Pro.local>
2018-07-19 11:49:55 +0200

    sort all

--- utils/index_bam.sh
+++ utils/index_bam.sh
@@ -5,14 +5,14 @@ sample=$(echo $bam | awk '{gsub(/.bam/, ""); print}')
 path2samtools=/hpc/hub_oudenaarden/bdebarbanson/bin/samtools-1.4.1
 sort_order=$(${path2samtools}/samtools view -H $bam | head -1 | cut -f 3 )
 
-if [[ $sort_order =~ 'unsorted' ]] ;
-then
+# if [[ $sort_order =~ 'unsorted' ]] ;
+# then
   echo "Sorting file into ${sample}.srt.bam"
   ${path2samtools}/samtools sort -@ 5  ${bam} -o ${sample}.srt.bam
   bam=${sample}.srt.bam
-else
-  echo "Bamfile is sorted"
-fi
+# else
+#   echo "Bamfile is sorted"
+# fi
 
 echo "Indexing file ${sample}.srt.bam"
 ${path2samtools}/samtools index -@ 5 $bam
