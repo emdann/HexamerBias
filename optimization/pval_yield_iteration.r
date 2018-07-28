70a2e13880468cab43f24e0bfce9245ca9e7d377
Emma <user@Changes-MacBook-Pro.local>
2018-07-28 11:55:42 +0200

    yield

--- optimization/pval_yield_iteration.r
+++ optimization/pval_yield_iteration.r
@@ -10,7 +10,7 @@ parser$add_argument("bestEvenTrack", type="character",
                     help = "RDS file of best and even coverage predicted SCALED coverage track (the whole thing or just a sample)")
 parser$add_argument("roiBed", type="character",
                     help = "BED file of regions on which to compare yields")
-parser$add_argument("-t","--threads" type="numeric",
+parser$add_argument("-t","--threads", type="numeric",
                     help = "Number of cores to use")
 args <- parser$parse_args()
 
@@ -28,7 +28,8 @@ roi.track <- import(ROI.track.file, format = 'BED')
 score <- delta.yield.permutation(scaled.track, roi.track, threads = cores)
 
 ## Write to file
+smp.name <- gsub(pattern = '.+/|\\.RDS', replacement = '', bestEven.track.file)
 roi.name <- gsub(pattern = '.+/|\\.bed', replacement = '', ROI.track.file)
-write(score, file = paste0('/hpc/hub_oudenaarden/edann/pred_coverage_primer_batch_D3R/evenNreads/coverage_yield/yield_pval.',roi.name,'.txt'), append = T)
+write(score, file = paste0('/hpc/hub_oudenaarden/edann/pred_coverage_primer_batch_D3R/evenNreads/coverage_yield/yield_pval.',smp.name,'.' ,roi.name,'.txt'), append = T)
 
 
