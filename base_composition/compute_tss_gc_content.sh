3497b5e9f519c260ba515b1543389c8f064d3901
Emma <user@Changes-MacBook-Pro.local>
2018-08-03 16:33:59 +0200

    add argument

--- base_composition/compute_tss_gc_content.sh
+++ base_composition/compute_tss_gc_content.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-refgen_fa=$1
-genome=$2
-slop=$3
+genes_bed=$1
+refgen_fa=$2
+genome=$3
 
 path2bedtools=/hpc/hub_oudenaarden/edann/bin/bedtools2/bin
 
@@ -11,7 +11,7 @@ cat $genes_bed | \
 	grep -v '_' > TxStart_${genes_bed}
 
 cat TxStart_${genes_bed} | \
-	${path2bedtools}/bedtools slop -b $slop -i stdin -g $genome | \
+	${path2bedtools}/bedtools slop -b 50 -i stdin -g $genome | \
 	${path2bedtools}/bedtools nuc -fi $refgen_fa} -bed stdin | \
 	cut -f 1,2,3,5 > TxStart_${genes_bed}.GCcont.bed
 
