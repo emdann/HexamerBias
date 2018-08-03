ad0cdabfeb3782873fbae20a02c755d94fcce983
Emma <user@Changes-MacBook-Pro.local>
2018-08-03 17:30:45 +0200

    da

--- base_composition/compute_tss_gc_content.sh
+++ base_composition/compute_tss_gc_content.sh
@@ -18,8 +18,8 @@ cat TxStart_${genes_bed} | \
 	${path2bedtools}/bedtools nuc -fi $refgen_fa -bed stdin | \
 	cut -f 1,2,3,5 > TxStart_${genes_bed}.GCcont.bed
 
-	cat TxStart_${genes_bed} | \
-		${path2bedtools}/bedtools slop -b $slop -i stdin -g $genome | \
-		${path2bedtools}/bedtools shuffle -chrom -i stdin -g $genome | \
-		${path2bedtools}/bedtools nuc -fi $refgen_fa -bed stdin | \
-		cut -f 1,2,3,5 > TxStart_shuffle_${genes_bed}.GCcont.bed
+cat TxStart_${genes_bed} | \
+${path2bedtools}/bedtools slop -b 50 -i stdin -g $genome | \
+${path2bedtools}/bedtools shuffle -chrom -i stdin -g $genome | \
+${path2bedtools}/bedtools nuc -fi $refgen_fa -bed stdin | \
+cut -f 1,2,3,5 > TxStart_shuffle_${genes_bed}.GCcont.bed
