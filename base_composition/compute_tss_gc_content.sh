61f41c3d98a7c09b45b51555067cd7f63e6166fd
Emma <user@Changes-MacBook-Pro.local>
2018-08-03 17:24:08 +0200

    smadk

--- base_composition/compute_tss_gc_content.sh
+++ base_composition/compute_tss_gc_content.sh
@@ -10,15 +10,16 @@ path2bedtools=/hpc/hub_oudenaarden/edann/bin/bedtools2/bin
 cat $genes_bed | \
 	cut -f 1,2,3 | \
 	awk '$3=$2{print}' | \
-	grep -v '_' > TxStart_${genes_bed}
+	grep -v '_' |
+	tr ' ' '\t' > TxStart_${genes_bed}
 
 cat TxStart_${genes_bed} | \
 	${path2bedtools}/bedtools slop -b 50 -i stdin -g $genome | \
-	${path2bedtools}/bedtools nuc -fi $refgen_fa} -bed stdin | \
+	${path2bedtools}/bedtools nuc -fi $refgen_fa -bed stdin | \
 	cut -f 1,2,3,5 > TxStart_${genes_bed}.GCcont.bed
 
 	cat TxStart_${genes_bed} | \
 		${path2bedtools}/bedtools slop -b $slop -i stdin -g $genome | \
 		${path2bedtools}/bedtools shuffle -chrom -i stdin -g $genome | \
-		${path2bedtools}/bedtools nuc -fi $refgen_fa} -bed stdin | \
+		${path2bedtools}/bedtools nuc -fi $refgen_fa -bed stdin | \
 		cut -f 1,2,3,5 > TxStart_shuffle_${genes_bed}.GCcont.bed
