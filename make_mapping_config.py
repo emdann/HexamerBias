240f1e9a67f5d20913314f05ceffe89941367d13
Emma <user@Changes-MacBook-Pro.local>
2018-08-22 18:24:08 +0200

    configfile

---
+++ make_mapping_config.py
@@ -0,0 +1,22 @@
+import json
+import os
+import argparse
+
+argparser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    description="Makes json config file for mapping snakemake. Has to be run from ")
+# argparser.add_argument('fasta', type=str, help='Fasta input')
+argparser.add_argument('-o', type=str, default='.', required=False, help='directory of fastq files (default: cwd)')
+argparser.add_argument('--clip5prime', type=str, default='9', required=False, help='number of bases to clip from 5 prime end (default: 9)')
+args = argparser.parse_args()
+
+inputDir=args.o
+clip=args.clip5prime
+
+samples = list(set([f.split('_R')[0] for f in inputDir if "fastq.gz" in f]))
+
+configDic = {}
+configDic["sample"]=samples
+configDic["clip5prime"] = clip
+
+with open('configfile.json', 'w') as outfile:
+    json.dump(configDic, outfile)
