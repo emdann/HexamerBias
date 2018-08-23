a8ae187c9af62b17917fc1163fd36461e223cc11
Emma <user@Changes-MacBook-Pro.local>
2018-08-23 12:09:34 +0200

    fixing

--- utils/remove_celSeq_contamination.py
+++ utils/remove_celSeq_contamination.py
@@ -2,7 +2,7 @@ import pysam as ps
 import re
 import argparse
 
-argparser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, 
+argparser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     description="Removes transcriptome contamination from raw reads from integrated Nla+CelSeq2 protocol. By Emma Dann")
 argparser.add_argument('fastq', type=str, help='Fastq input file')
 args = argparser.parse_args()
@@ -11,9 +11,8 @@ args = argparser.parse_args()
 fastqFile=args.fastq
 outname = fastqFile.split('.fastq')[0]+".noCelSeq.fastq.gz"
 
-infile = ps.FastxFile(fastqFile, "r")
-outfile = ps.FastxFile('ciao.fastq', 'w', template=infile)
-
-for entry in infile:
-    if "T"*15 not in entry.sequence:
-        outfile.write(entry)
+with ps.FastxFile(fastqFile) as infile, open(outname, mode ="w") as outfile:
+    for entry in infile:
+        if "T"*15 not in entry.sequence:
+            # print(entry.sequence)
+            outfile.write(str(entry) + "\n")
